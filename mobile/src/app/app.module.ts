cc3dc5fe865649dea6bf09f26756358cfb6586d3
Jack wangzhe <674112795@qq.com>
2019-04-30 22:56:46 +0800

    完成用户登录页面

--- mobile/src/app/app.module.ts
+++ mobile/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgZorroAntdMobileModule } from 'ng-zorro-antd-mobile';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
